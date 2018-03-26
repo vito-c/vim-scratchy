14cfab4d4a21484a8b533e34377be0ddd016c20e
GodFather <vito.blog@gmail.com>
2018-03-25 18:35:57 -0700

    working with real time changes

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"time"
@@ -62,9 +63,17 @@ type BufferOptions struct {
 	BufferType string
 }
 
+type jqRun struct {
+	nvim *nvim.Nvim
+	forceSetup bool
+	showError bool
+	args []string
+}
+
 func (v *Svim) NewBufferOpts(options BufferOptions) SBuffer {
 	v.Command(options.Location) // tab, vert, horz, default
 	buff, _ := v.CurrentBuffer()
+	log.Println("new buff: ", buff, " name: ", options.Name)
 	v.SetBufferName(buff, options.Name)
 	v.SetBufferOption(buff, "buftype", options.BufferType)
 	return SBuffer{buff, v, [][]byte{}}
@@ -140,7 +149,6 @@ func setup(n *nvim.Nvim, args []string) (string, error) { // Declare first arg a
 	v.Command("set syntax=json")
 	outWin, _ = v.CurrentWindow()
 	log.Println("outWin ", outWin)
-	// v.Command("setlocal bt=nofile bh=wipe noma nomod nonu nobl nowrap ro nornu")
 
 	/*** Filter Buffer ***/
 	jqbuff = v.NewBufferOpts(BufferOptions{
@@ -151,26 +159,27 @@ func setup(n *nvim.Nvim, args []string) (string, error) { // Declare first arg a
 		Name:       "[jq]",
 		BufferType: nofile,
 	})
+	log.Println("jqbuff: ", jqbuff.Buffer)
 	v.Command("set syntax=jq")
-	jqWin, _ = v.CurrentWindow()
-	log.Println("jqWin ", jqWin)
 
 	// Finish setup
 	v.SetCurrentBuffer(databuff.Buffer)
+	dataWin, _ = v.CurrentWindow()
+	log.Println("dataWin ", dataWin)
 	v.Command("split")
 	v.Command("wincmd K")
-	dataWin, _ = v.CurrentWindow()
-	v.SetWindowHeight(dataWin, 10)
-	log.Println("dataWin ", dataWin)
+	jqWin, _ = v.CurrentWindow()
+	v.SetWindowHeight(jqWin, 10)
+	log.Println("dataWin ", jqWin)
 
 	v.SetCurrentBuffer(jqbuff.Buffer)
-	// v.SetBufferAuCmd("TextChangedI", jqbuff, "ScratchyRun")
-	// v.SetBufferAuCmd("TextChanged", jqbuff, "ScratchyRun")
 	v.SetCurrentLine([]byte("map(.url)"))
 	log.Printf("jqbuff %v\n", jqbuff)
 	log.Printf("databuff %v\n", databuff)
 	log.Printf("outbuff %v\n", outbuff)
-	return scratchyRun(v, args)
+	v.SetBufferAuCmd("TextChangedI", jqbuff.Buffer, "call ScratchyRun()")
+	v.SetBufferAuCmd("TextChanged", jqbuff.Buffer, "call ScratchyRun()")
+	return scratchyRun(n, false, true, args)
 }
 
 func (r *SBuffer) getString() string {
@@ -183,33 +192,69 @@ func (r *SBuffer) getString() string {
 	return string(buff)
 }
 
-func scratchyRun(v Svim, args []string) (string, error) { // Declare first arg as *nvim.Nvim to get current client
-	if jqbuff.Buffer == databuff.Buffer {
+var jqr *jq.JQ
+
+func scratchyRun(
+	n *nvim.Nvim,
+	forceSetup bool,
+	showError bool,
+	args []string,
+) (string, error) { // Declare first arg as *nvim.Nvim to get current client
+	v := Svim{n}
+	if (jqbuff.Buffer == databuff.Buffer) || forceSetup {
 		return setup(v.Nvim, args)
 	}
 
 	json := databuff.getString()
 	query := jqbuff.getString()
 
-	jqr := &jq.JQ{
+	jqr = &jq.JQ{
 		J: string(json),
 		Q: string(query),
 	}
 
-	v.SetCurrentWindow(dataWin)
-	log.Println("dataWin ", dataWin)
+	v.SetCurrentWindow(outWin)
+	v.SetCurrentBuffer(outbuff.Buffer)
+	v.Command("setlocal bt=nofile bh=wipe ma mod nonu nobl nowrap noro nornu")
+	log.Println("-------------------")
+	if err := jqr.Validate(); err != nil {
+		log.Println("ERR: ", err.Error())
+		return err.Error(), nil
+	}
+	log.Println("-------------------")
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
-	if err := jqr.Eval(ctx, &outbuff); err != nil {
-		fmt.Fprint(&outbuff, err.Error())
-		log.Fatalln(err.Error())
+
+	log.Println(jqr.String())
+	if err := jqr.Eval(ctx, &outbuff, ioutil.Discard); err != nil {
+		if showError {
+			v.Command("echom \"" + err.Error() + "\"")
+		}
+		log.Println(err.Error())
 	}
+	v.Command("setlocal bt=nofile bh=wipe noma nomod nonu nobl nowrap ro nornu")
+	v.SetCurrentWindow(jqWin)
 
 	return "finished", nil
 }
 
+// func JqTextChanged(jqr jqRun) (string, error) { // Declare first arg as *nvim.Nvim to get current client
+// 	scratchyRun(jqr.nvim, jqr.forceSetup, jqr.showError, jqr.
+// }
+//
+// func JqTextChangedI() {
+// }
+
+func (v *Svim) RemoveBufferAuCmds() {
+	return
+}
+
 func (v *Svim) SetBufferAuCmd(aucmd string, buffer nvim.Buffer, cmd string) {
-	v.Command(fmt.Sprintf("au %v <buffer=%v> %v", aucmd, buffer, cmd))
+	str := fmt.Sprintf("au %s <buffer=%d> %s", aucmd, buffer, cmd)
+	log.Println("cmd: ", str)
+	if err := v.Command(str); err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func (r *SBuffer) eof() error {
@@ -254,11 +299,7 @@ func (b *SBuffer) Read(p []byte) (n int, err error) {
 }
 
 func (b *SBuffer) Write(p []byte) (n int, err error) {
-	// t, _ := b.nvim.BufferLineCount(b.Buffer)
-	// log.Println("WRITE ------------ ", t)
-	// log.Println(string(p))
-	log.Println("------------ ", len(p))
-	// b.data = append(b.data, p)
+	log.Println("Do Writer: ", string(p))
 
 	if len(p) == 0 {
 		return 0, nil
@@ -267,7 +308,9 @@ func (b *SBuffer) Write(p []byte) (n int, err error) {
 	err = b.nvim.SetBufferLines(
 		b.Buffer,
 		0, -1, true, bytes.Split(p, []byte{'\n'}))
-	log.Println("------------")
+	if err != nil {
+		log.Println("write error: ", err.Error())
+	}
 	return len(p), err
 }
 
